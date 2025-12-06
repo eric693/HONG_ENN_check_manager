a6f1c7ee733f656599c2ed485a83c16162fbddcc
eric693 <exo881222@gmail.com>
2025-12-06 14:25:31 +0800

    update

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbwQeSXcQUO4zgazZb9yxq6EvA1mcmtZTWpuv-LWTApIirGOG_7OGTIYx3beLjGNKz8fgQ/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxAKhFn4o1C4deqRZmxM6-ioBEW2Eo9JLrrysUJmgn1oTPMChL5dJULAlAGXYsZesWZzg/exec",
   
   // 新增回呼網址
-  redirectUrl: "https://eric693.github.io/New_Attendance-System/"
+  redirectUrl: "https://eric693.github.io/HONG_ENN_check_manager/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v4.2.2'
